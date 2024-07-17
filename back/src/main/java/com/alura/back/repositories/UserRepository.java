457c50b467e761a697506f15b067f92ca25ff810
Felipe Munevar N <felipemunevarn@gmail.com>
2024-07-17 01:32:48 -0300

    all layers for users

---
+++ back/src/main/java/com/alura/back/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.alura.back.repositories;
+
+import com.alura.back.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
---
+++ back/src/main/java/com/alura/back/services/UserService.java
@@ -0,0 +1,25 @@
+package com.alura.back.services;
+
+import com.alura.back.entities.User;
+import com.alura.back.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+}
