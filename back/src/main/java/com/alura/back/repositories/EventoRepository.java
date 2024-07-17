3cabd02770257a2f3d9a16bfaa318edcfe9a4591
Clever Machaca <106214364+cleverjohann@users.noreply.github.com>
2024-07-17 12:28:08 -0500

    Implementacion de Controller y Swagger
    Se añadieron el controlador, servicio y repositorio. Las dependencias y configuración de Swagger

---
+++ back/src/main/java/com/alura/back/repositories/EventoRepository.java
@@ -0,0 +1,9 @@
+package com.alura.back.repositories;
+
+import com.alura.back.entities.Evento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventoRepository extends JpaRepository<Evento, Long> {
+}
