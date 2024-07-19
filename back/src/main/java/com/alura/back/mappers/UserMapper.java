b9d9525c9bf8f37640d8d00a1aabe227f3836cf3
Felipe Munevar N <felipemunevarn@gmail.com>
2024-07-18 22:57:53 -0300

    complete all methods for CRUD of user

--- back/src/main/java/com/alura/back/mappers/UserMapper.java
+++ back/src/main/java/com/alura/back/mappers/UserMapper.java
@@ -1,5 +1,6 @@
 package com.alura.back.mappers;
 
+import com.alura.back.Dtos.requestDto.UserRequestDto;
 import com.alura.back.Dtos.responseDto.UserResponseDto;
 import com.alura.back.entities.User;
 import org.springframework.stereotype.Component;
@@ -22,8 +23,19 @@ public class UserMapper {
                 user.getDevType(),
                 user.getRole()
         );
-
         return dto;
+    }
+
+    public User toUser(UserRequestDto dto) {
+        User user = new User();
+        user.setFirstName(dto.getFirstName());
+        user.setLastName(dto.getLastName());
+        user.setDateOfBirths(dto.getDateOfBirths());
+        user.setGender(dto.getGender());
+        user.setEmail(dto.getEmail());
+        user.setDevType(dto.getDevType());
+        user.setRole(dto.getRole());
+        return user;
     }
 
     public List<UserResponseDto> toUserResponseDtos(List<User> users) {
--- back/src/main/java/com/alura/back/services/implementService/UserServiceImpl.java
+++ back/src/main/java/com/alura/back/services/implementService/UserServiceImpl.java
@@ -1,15 +1,18 @@
 package com.alura.back.services.implementService;
 
+import com.alura.back.Dtos.requestDto.UserRequestDto;
 import com.alura.back.Dtos.responseDto.UserResponseDto;
 import com.alura.back.entities.User;
 import com.alura.back.mappers.UserMapper;
 import com.alura.back.repositories.UserRepository;
 import com.alura.back.services.interfaceService.IUserService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 //@RequiredArgsConstructor
@@ -28,5 +31,43 @@ public class UserServiceImpl implements IUserService {
     public List<UserResponseDto> getUsers() {
         List<User> users = userRepository.findAll();
         return userMapper.toUserResponseDtos(users);
+    }
+
+    @Override
+    public UserResponseDto getUserById(Long id) {
+        Optional<User> user = userRepository.findById(id);
+        return user.map(userMapper::toUserResponseDto).orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public UserResponseDto createUser(UserRequestDto userRequestDto) {
+        User user = userMapper.toUser(userRequestDto);
+        user = userRepository.save(user);
+        return userMapper.toUserResponseDto(user);
+    }
+
+    @Override
+    @Transactional
+    public UserResponseDto updateUser(Long id, UserRequestDto userRequestDto) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            user.setFirstName(userRequestDto.getFirstName());
+            user.setLastName(userRequestDto.getLastName());
+            user.setDateOfBirths(userRequestDto.getDateOfBirths());
+            user.setGender(userRequestDto.getGender());
+            user.setEmail(userRequestDto.getEmail());
+            user.setDevType(userRequestDto.getDevType());
+            user.setRole(userRequestDto.getRole());
+            user = userRepository.save(user);
+            return userMapper.toUserResponseDto(user);
+        }
+        return null;
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
     }
 }
